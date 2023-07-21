0d4bc9aebb32eb96789f98b1884c3e1654bdedd1
Yuuna <k9532121@gmail.com>
2023-07-21 14:57:56 +0800

    feat: 🎸 選單列增加 Pokedex 與 Items 項目

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -6,21 +6,24 @@ export default function Sidebar() {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.logo + " fz-10"}>
+        <Link href='/' className={styles.logo + " fz-10"}>
           <Image src="/images/PokemonDashboard-logo.png" alt="PokemonDashboard-logo" width={370} height={50} />
           {/* TODO: 製作 LOGO */}
-        </div>
-        {/* <div className={styles.menu}>
-          <Link href="/" className={styles.menuItem}>
+        </Link>
+        <div className={styles.menu}>
+          {/* <Link href="/" className={styles.menuItem}>
             首頁
-          </Link>
-          <Link href="/Pokedex/index" className={styles.menuItem}>
+          </Link> */}
+          <Link href="/" className={styles.menuItem}>
             Pokédex
           </Link>
-          <Link href="/about" className={styles.menuItem}>
-            關於我
+          <Link href="/items" className={styles.menuItem}>
+            Items
           </Link>
-        </div> */}
+          {/* <Link href="/about" className={styles.menuItem}>
+            關於我
+          </Link> */}
+        </div>
       </header>
     </>
   )
---
+++ pages/items.tsx
@@ -0,0 +1,7 @@
+export default function Items() {
+  return (
+    <>
+      <div style={{textAlign: 'center', width: '100%'}}>頁面施工中！！</div>
+    </>
+  )
+}
