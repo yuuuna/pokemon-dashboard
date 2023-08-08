c75aee439c2b606c0331df0c08098d19dafbf537
Yuuna <k9532121@gmail.com>
2023-08-08 22:18:10 +0800

    refactor: 💡 移除未使用的 api 項目

--- src/api.tsx
+++ src/api.tsx
@@ -8,17 +8,7 @@ export const fetchPokemonData = async ({name} : any) => {
     return data;
 };
 
-export const fetchItemList = async ({offset, limit} : any) => {
-    const data = fetch(`https://pokeapi.co/api/v2/item?offset=${offset}&limit=${limit}`).then((res) => res.json());
-    return data;
-};
-
 export const fetchItemCategory = async ({name} : any) => {
     const data = fetch(`https://pokeapi.co/api/v2/item-category/${name}`).then((res) => res.json());
-    return data;
-};
-
-export const fetchItemData = async ({name} : any) => {
-    const data = fetch(`https://pokeapi.co/api/v2/item/${name}`).then((res) => res.json());
     return data;
 };
